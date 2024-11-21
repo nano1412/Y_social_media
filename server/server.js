9784ea007396db7ee5f6a420303ddc423f9e29fe
nano1412 <54862667+nano1412@users.noreply.github.com>
2024-11-22 06:18:58 +0700

    reimport module

---
+++ server/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const hostname = 'localhost';
+const port = 3000;
+const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+const multer = require('multer');
+const path = require('path');
+const mysql = require('mysql');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+      callback(null, 'public/img/');
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+
+const imageFilter = (req, file, cb) => {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "Nano1412",
+    password: "154712349876kK!",
+    database: "Y_Social_Media"
+})
+
+con.connect(err => {
+    if(err) throw(err);
+    else{
+        console.log("MySQL connected");
+    }
+})
+
+const queryDB = (sql) => {
+    return new Promise((resolve,reject) => {
+        // query method
+        con.query(sql, (err,result, fields) => {
+            if (err) reject(err);
+            else
+                resolve(result)
+        })
+    })
+}
+
+//bottom
+app.listen(port, hostname, () => {
+    console.log(`Server running at   http://${hostname}:${port}/register.html`);
+});
