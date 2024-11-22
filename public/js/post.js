e3a56760f8e664800daa96e40dda72be3aa288fa
SwordTomato <122582074+SwordTomato@users.noreply.github.com>
2024-11-22 20:30:40 +0700

    test in post.js

--- public/js/post.js
+++ public/js/post.js
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>post</title>
-		<link type="text/css" rel="stylesheet" href="css/style.css">
-		<script type="text/javascript" src="js/post.js"></script>
-	</head>
-	<body>
-		
-	
-	</body>
-</html>
+// test
